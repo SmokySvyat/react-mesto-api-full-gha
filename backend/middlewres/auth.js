8ae29fc9b61453fef1fca7526e743956f099162a
SvyatLinux <swcnova@gmail.com>
2023-09-05 20:44:06 +0300

    fix auth5

--- backend/middlewres/auth.js
+++ backend/middlewres/auth.js
@@ -5,12 +5,12 @@ const ErrorAccess = require('../utils/errors/ErrorAccess');
 const handleAuthError = (req, res, next) => next(new ErrorAccess('Необходима авторизация'));
 // eslint-disable-next-line consistent-return
 const auth = (req, res, next) => {
-  const { token } = req.cookies;
+  const { token } = req.headers.authorization;
   try {
     if (!token) {
       return handleAuthError(req, res, next);
     }
-    const payload = jwt.verify(token, JWT_SECRET);
+    const payload = jwt.verify(token.replace('Bearer ', ''), JWT_SECRET);
     req.user = payload;
     next();
   } catch (err) {

0ee75e6c359e70e5b1fdf1c7527f1d72376abb97
SvyatLinux <swcnova@gmail.com>
2023-09-05 21:07:36 +0300

    auth6 login

--- backend/middlewres/auth.js
+++ backend/middlewres/auth.js
@@ -4,20 +4,29 @@ const ErrorAccess = require('../utils/errors/ErrorAccess');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
-const handleAuthError = (req, res, next) => next(new ErrorAccess('Необходима авторизация'));
+const HandleAuthError = (req, res, next) => next(new ErrorAccess('Необходима авторизация'));
 // eslint-disable-next-line consistent-return
 const auth = (req, res, next) => {
-  const { token } = req.cookies;
-  try {
-    if (!token) {
-      return handleAuthError(req, res, next);
-    }
-    const payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'secret-key');
-    req.user = payload;
-    next();
-  } catch (err) {
-    return handleAuthError(req, res, next);
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    next(new HandleAuthError('Необходимо авторизоваться'));
+    return;
   }
+
+  const token = authorization.replace('Bearer ', '');
+
+  let payload;
+
+  try {
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'secret-key');
+  } catch (err) {
+    next(new HandleAuthError('Необходимо авторизоваться'));
+    return;
+  }
+
+  req.user = payload;
+  next();
 };
 
 module.exports = auth;

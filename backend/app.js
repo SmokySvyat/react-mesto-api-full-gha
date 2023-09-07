e35235e8e20c8a46c5668fdb023fc2d117125f9e
SvyatLinux <swcnova@gmail.com>
2023-09-07 19:09:46 +0300

    1.5 iteration: another auth

--- backend/app.js
+++ backend/app.js
@@ -6,14 +6,14 @@ const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
 const errorHandler = require('./middlewres/error');
-// const router = require('./routes');
+const router = require('./routes');
 const { requestLogger, errorLogger } = require('./middlewres/logger');
 
-const auth = require('./middlewres/auth');
-const cardsRouter = require('./routes/cards');
-const usersRouter = require('./routes/users');
-const authRouter = require('./routes/auth');
-const NotFound = require('./utils/errors/NotFound');
+// const auth = require('./middlewres/auth');
+// const cardsRouter = require('./routes/cards');
+// const usersRouter = require('./routes/users');
+// const authRouter = require('./routes/auth');
+// const NotFound = require('./utils/errors/NotFound');
 
 const app = express();
 app.use(helmet());
@@ -23,21 +23,21 @@ app.use(requestLogger);
 app.use(bodyParser.json());
 app.use(cookieParser({ origin: ['http://localhost:3000', 'mesto.svyat.nomoredomainsicu.ru'], credentials: true }));
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
+// app.get('/crash-test', () => {
+//   setTimeout(() => {
+//     throw new Error('Сервер сейчас упадёт');
+//   }, 0);
+// });
 
-app.use('/', authRouter);
-app.use('/cards', auth, cardsRouter);
-app.use('/users', auth, usersRouter);
+// app.use('/', authRouter);
+// app.use('/cards', auth, cardsRouter);
+// app.use('/users', auth, usersRouter);
 
-app.use('*', (req, res, next) => {
-  next(new NotFound('Такой страницы не существует'));
-});
+// app.use('*', (req, res, next) => {
+//   next(new NotFound('Такой страницы не существует'));
+// });
 
-// app.use(router);
+app.use(router);
 
 app.use(errorLogger);
 
